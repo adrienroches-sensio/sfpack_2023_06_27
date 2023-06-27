abe320e825d713b20ed480db40219e2e5acaba9d
Adrien Roches <adrien@roc-it.tech>
2023-06-27 15:34:34 +0200

    [sfstart/02-twig] Add the list and movie details routes

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -2,17 +2,37 @@
 
 namespace App\Controller;
 
+use App\Model\MovieRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MovieController extends AbstractController
 {
-    #[Route('/movie', name: 'app_movie')]
-    public function index(): Response
+    #[Route(
+        path: '/movies',
+        name: 'app_movies_list',
+        methods: ['GET'],
+    )]
+    public function list(): Response
     {
-        return $this->render('movie/index.html.twig', [
-            'controller_name' => 'MovieController',
+        return $this->render('movie/list.html.twig', [
+            'movies' => MovieRepository::listAll(),
+        ]);
+    }
+
+    #[Route(
+        path: '/movies/{slug}',
+        name: 'app_movies_details',
+        requirements: [
+            'slug' => '\d{4}-\w([-\w])*'
+        ],
+        methods: ['GET'],
+    )]
+    public function details(string $slug): Response
+    {
+        return $this->render('movie/details.html.twig', [
+            'movie' => MovieRepository::getBySlug($slug),
         ]);
     }
 }
