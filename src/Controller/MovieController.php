6165293cd2d56f142d2804de373e530cd38ea296
Adrien Roches <adrien@roc-it.tech>
2023-06-30 14:47:26 +0200

    [sfmaster/04-event] Save lastLoggedInAt on USer class with EventSubscriber

--- src/Controller/MovieController.php
+++ src/Controller/MovieController.php
@@ -12,7 +12,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
 
 class MovieController extends AbstractController
 {
--- src/EventSubscriber/LoginSubscriber.php
+++ src/EventSubscriber/LoginSubscriber.php
@@ -2,20 +2,39 @@
 
 namespace App\EventSubscriber;
 
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Psr\Clock\ClockInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\Security\Http\Event\LoginSuccessEvent;
 
 class LoginSubscriber implements EventSubscriberInterface
 {
-    public function onLoginSuccessEvent(LoginSuccessEvent $event): void
-    {
-        // ...
+    public function __construct(
+        private readonly ClockInterface $clock,
+        private readonly UserRepository $userRepository,
+    ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            LoginSuccessEvent::class => 'onLoginSuccessEvent',
+            LoginSuccessEvent::class => [
+                ['saveLastLoginSuccessDate', 0],
+            ],
         ];
+    }
+
+    public function saveLastLoginSuccessDate(LoginSuccessEvent $event): void
+    {
+        $user = $event->getUser();
+
+        if (!$user instanceof User) {
+            return;
+        }
+
+        $user->setLastLoggedInAt($this->clock->now());
+
+        $this->userRepository->save($user, true);
     }
 }
